914be22133ad12ce314ee004d173f4ca3edec822
Aman Sahani <aman.sahani@gmail.com>
2016-10-24 19:48:56 +1100

    updated task model and made some UI testing for tasks in sidebar

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,31 @@
 class Task < ActiveRecord::Base
+  ##################################
+  # Relationships
+  ##################################
+
+  ##################################
+  # Validations
+  ##################################
+  validates_presence_of :title
+  validates_numericality_of :priority, :allow_nil => true
+  validates_numericality_of :budget_id, :allow_nil => true
+
+  ##################################
+  # Callbacks
+  ##################################
+
+  ##################################
+  # Scoped Methods
+  ##################################
+
+  ##################################
+  # Class Methods
+  ##################################
+
+  ##################################
+  # Instance Methods
+  ##################################
+  def age
+    Time.now.to_date - self.created_at.to_date
+  end
 end
