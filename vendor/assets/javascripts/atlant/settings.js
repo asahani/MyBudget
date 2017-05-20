0e4279d64a36fbaf190020f4b57b143b10dae2d3
Aman Sahani <aman.sahani!@gmail.com>
2017-05-21 03:18:49 +1000

    New dashboard layout

--- vendor/assets/javascripts/atlant/settings.js
+++ vendor/assets/javascripts/atlant/settings.js
@@ -59,9 +59,9 @@ $(document).ready(function(){
 
     /* Default settings */
     var theme_settings = {
-        st_head_fixed: 0,
+        st_head_fixed: 1,
         st_sb_fixed: 1,
-        st_sb_scroll: 1,
+        st_sb_scroll: 0,
         st_sb_right: 0,
         st_sb_custom: 0,
         st_sb_toggled: 0,
