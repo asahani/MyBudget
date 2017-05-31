7a058c277762de8ed05bd8d033673a71da000225
Aman Sahani <aman.sahani!@gmail.com>
2017-05-31 19:25:54 +1000

    Fixed dashboard budget progress bar

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,5 +6,11 @@ class DashboardController < ApplicationController
     @annual_report = get_annual_report(@budget_year)
     @goals = Goal.all.active
     @budget = Budget.where('start_date <= ? and end_date >= ?',Date.today,Date.today).first
+    @budget_consumption = 0
+    unless @budget.nil?
+      @budget_consumption = @budget.budget_consumption_percentage
+    end
+    puts 'Budget Consumption = '
+    puts @budget_consumption
   end
 end
